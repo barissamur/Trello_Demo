1849c21391f8a821cf451604e602dcae32c6e44c
Hasan Barış Samur <barissamurr@gmail.com>
2024-03-05 22:14:49 +0300

    ardalis repo added

---
+++ Tello_Demo.Application/Interfaces/IRepo.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tello_Demo.Application.Interfaces;
+
+public interface IRepo<T> : IRepositoryBase<T> where T : class
+{
+}
---
+++ Tello_Demo.Infrastructure/Repositories/EFRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using Tello_Demo.Application.Interfaces;
+using Tello_Demo.Infrastructure.Context;
+
+namespace Tello_Demo.Infrastructure.Repositories;
+
+public class EFRepository<T> : RepositoryBase<T>, IReadRepositoryBase<T>, IRepo<T> where T : class
+{
+    public EFRepository(AppDbContext dbcContext) : base(dbcContext)
+    {
+
+    }
+}
